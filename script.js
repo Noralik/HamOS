f25d3cccc91809b021eb6ef11d864b9fd063a64f
Noralik <dragon11time11play@gmail.com>
2025-05-20 16:12:44 +0300

    sigma uwu

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('Hello!');
+setInterval(function () {
+    const img = document.createElement('img');
+    const allImg = document.querySelectorAll('img');
+    img.src = 'kafka.png';
+    for (let i = 0; i < allImg.length; i++) {
+      allImg[i].style.top = randomNumber(3, 97) + '%';
+      allImg[i].style.left = randomNumber(3, 97) + '%';
+    }
+  
+    img.addEventListener('click', function () {
+      img.style.transform = 'scale(1.1) rotate(360deg)';
+      document.body.style.backgroundColor = 'yellow';
+      setTimeout(function () {
+        img.remove();
+        document.body.style.backgroundColor = 'white';
+      }, 200);
+    });
+  
+    document.body.append(img);
+  }, 200);
+  
+  document.addEventListener("keypress", function(){
+    const allImg = document.querySelectorAll('img');
+    for (let i = 0; i < allImg.length; i++) {
+        allImg[i].remove();
+      }
+  });
+  
+  const randomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+  
