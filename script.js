bb4df9b8411fce3673189395df8329bc988ecfd3
Noralik <158551209+Noralik@users.noreply.github.com>
2025-05-27 16:14:10 +0300

    Duck

---
+++ script.js
@@ -0,0 +1,29 @@
+setInterval(function () {
+  const img = document.createElement('img');
+  const allImg = document.querySelectorAll('img');
+  img.src = 'duck.png';
+  for (let i = 0; i < allImg.length; i++) {
+    allImg[i].style.top = randomNumber(3, 97) + '%';
+    allImg[i].style.left = randomNumber(3, 97) + '%';
+  }
+
+  img.addEventListener('click', function () {
+    img.style.transform = 'scale(1.1) rotate(360deg)';
+    document.body.style.backgroundColor = 'yellow';
+    for (let i = 0; i < allImg.length; i++) {
+      allImg[i].remove();
+    }
+    setTimeout(function () {
+      img.remove();
+      document.body.style.backgroundColor = 'white';
+    }, 200);
+  });
+
+  document.body.append(img);
+}, 200);
+
+document;
+
+const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
