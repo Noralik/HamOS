89e636912b88a947df0f4cb0f9c3e9269c95ab54
Noralik <dragon11time11play@gmail.com>
2025-06-02 16:45:03 +0300

    v2

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
 const playerField = document.querySelector('.playerField');
-const enemyField = document.querySelector('.enemyField');
-let playerShips = 0
-const shipSet = []
-const ComputerShipSet = []
+const enemyField = document.querySelector('.EnemyField');
+let playerShips = 0;
+const shipSet = [];
+const computerShipSet = [];
+const computerShips = [];
+const userShips = [];
 
+const randomFunction = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 
-const createBox = (number, parentElem) => {
+const genearteComputerShips = (shipsArr) => {
+  for (let i = 0; i < 3; i++) {
+    let randomShip = randomFunction(0, 12);
+    if (computerShips.includes(randomShip)) {
+      shipsArr.push(randomShip);
+    } else {
+      while (computerShips.includes(randomShip)) {
+        randomShip = randomFunction(0, 12);
+      }
+      shipsArr.push(randomShip);
+    }
+  }
+};
+
+genearteComputerShips(computerShips);
+
+const createBox = (number, parentElem, states, isComputer) => {
   for (let i = 0; i < number; i++) {
+    states.push(false);
     const gameBlock = document.createElement('div');
     gameBlock.classList.add('block');
-        gameBlock.addEventListener('click', function () {
-            if (playerShips < 3 && shipSet[i] != true){
-            createImg('1307865.png', gameBlock);
-            playerShips++;
-            shipSet[i] = true;
+    if (!isComputer) {
+      gameBlock.addEventListener('click', function () {
+        if (playerShips < 3 && shipSet[i] != true) {
+          createImg('1307865.png', gameBlock);
+          playerShips++;
+          userShips.push(i);
+          shipSet[i] = true;
         }
-    });
+      });
+    } else {
+      gameBlock.addEventListener('click', function () {
+        if (shipSet[i] != true && computerShips.includes(i)) {
+          createImg('111160871.png', gameBlock);
+        }
+        computerShipSet[i] = true;
+        let computerSelectedShip = randomFunction(0, 12);
+        if(userShips.includes(computerSelectedShip)) {
+            gameBlock.src = 'brokenship.jpg';
+            userShips.slice(userShips.indexOf(computerSelectedShip), 1)
+        }
+      });
+    }
     parentElem.appendChild(gameBlock);
+  }
 };
-}
 
 const createImg = (link, parent) => {
   const gameImg = document.createElement('img');
@@ -27,5 +64,5 @@ const createImg = (link, parent) => {
   parent.appendChild(gameImg);
 };
 
-createBox(12, playerField, shipSet);
-createBox(12, enemyField, ComputerShipSet);
+createBox(12, playerField, shipSet, false);
+createBox(12, enemyField, computerShipSet, true);
